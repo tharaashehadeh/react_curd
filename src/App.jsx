b5de163fe9edb28e065adf03c16c20991839fd33
unknown <Tharashehadeh@gmail.com>
2023-11-16 12:55:16 +0200

    react curd

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
-import Index from './components/users/index.jsx'
 import Create from './components/users/Create.jsx'
 import Details from './components/users/Details.jsx'
 import Edit from './components/users/Edit.jsx'
+import Index from './components/users/Index.jsx'
 export default function App() {
   return (
 <>
